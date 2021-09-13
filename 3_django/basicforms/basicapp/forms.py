7137f46a796310b60bebe04eba91a93f7b51df95
aglebionek <aglebionek@gmail.com>
2021-09-13 17:15:09 +0200

    django forms validation basics

--- 3_django/basicforms/basicapp/forms.py
+++ 3_django/basicforms/basicapp/forms.py
@@ -1,6 +1,31 @@
 from django import forms
+from django.core import validators
+
+#example of custom validator, you would pass it to a field with validators=[chech_for_z]
+def check_for_z(value:str):
+    if value.lower() != 'z':
+        raise forms.ValidationError("Name needs to start with Z")
 
 class FormName(forms.Form):
     name = forms.CharField()
     email = forms.EmailField()
+    verify_email = forms.EmailField(label='enter email again')
     text = forms.CharField(widget=forms.Textarea())
+    botcatcher = forms.CharField(required=False, widget=forms.HiddenInput(), validators=[validators.MaxLengthValidator(0)])
+
+    def clean(self):
+        all_clean_data = super().clean()
+        email = all_clean_data['email']
+        vemail = all_clean_data['verify_email']
+        if email != vemail: raise forms.ValidationError("Emails don't match")
+
+    '''
+    This is what the validator in botcatcher is doing now
+
+    def clean_botcatcher(self):
+    botcatcher = self.cleaned_data['botcatcher']
+    if(len(botcatcher)):
+        raise forms.ValidationError("Bot alert")
+    '''
+
+    
--- 3_django/basicforms/basicforms/settings.py
+++ 3_django/basicforms/basicforms/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-from settings_private import secret_key
+from basicforms.settings_private import secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
